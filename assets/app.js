62da4affc6af5d2b9329d74266bc6335c424213d
DoJeon1 <donghwan.dj@gmail.com>
2021-05-20 00:16:03 -0600

    added dropdown menu in main body

--- assets/app.js
+++ assets/app.js
@@ -2,5 +2,22 @@
 	"use strict";
 
 	// Add Scripts Here
+	function dropDown() {
+		document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown__content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
     	
 })();
