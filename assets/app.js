4a75578dbd0be5e6ce55decc4f17aa8839ca290e
DoJeon1 <donghwan.dj@gmail.com>
2021-05-21 07:44:52 -0600

    formated mobile header menu

--- assets/app.js
+++ assets/app.js
@@ -20,22 +20,11 @@ window.onclick = function(event) {
 }
 
 function dropDownMobile() {
-  "use strict";
-  document.getElementById("dropDownMobile").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  "use strict";
-  if (!event.target.matches('.dropbtnMobile')) {
-    var dropdowns = document.getElementsByClassName("nav__mobile");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+  var x = document.getElementById("dropdownMobile");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
     	
